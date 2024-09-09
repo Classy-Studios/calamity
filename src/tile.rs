edacdf5c757275b45f92af6be7dd246608c0cf63
ThiccTheo <th3o1237@gmail.com>
2024-09-09 12:13:01 -0400

    moved some stuff around

--- src/tile.rs
+++ src/tile.rs
@@ -1,5 +1,5 @@
 use {
-    super::{asset_owner::TextureAtlasOwner, GameState},
+    super::{asset_owner::TextureAtlasOwner, level::LevelObject, GameState},
     bevy::prelude::*,
     bevy_rapier2d::prelude::*,
 };
@@ -9,93 +9,16 @@ const WALL_THICKNESS: f32 = 15.;
 const HALF_WALL_THICKNESS: f32 = WALL_THICKNESS / 2.;
 const HALF_TILE_SIZE: f32 = (TILE_SIZE.x + TILE_SIZE.y) / 2. / 2.;
 
-#[repr(usize)]
-#[derive(Copy, Clone)]
-pub enum TileIndex {
-    Grass1 = 0,
-    Grass2 = 1,
-    Grass3 = 2,
-    Grass4 = 3,
-    Dirt1 = 4,
-    Dirt2 = 5,
-    Gravel1 = 6,
-    Gravel2 = 7,
-    Gravel3 = 8,
-    Gravel4 = 9,
-    Water1 = 18,
-    Water2 = 19,
-    Floor1 = 95,
-    Floor2 = 96,
-    BrokenFloor1 = 98,
-    BrokenFloor2 = 99,
-    BrokenFloor3 = 100,
-    SinglePillow = 101,
-    SingleMiddle = 74,
-    SingleFoot = 47,
-    DoublePillow1 = 102,
-    DoublePillow2 = 103,
-    DoubleMiddle1 = 75,
-    DoubleMiddle2 = 76,
-    DoubleFoot1 = 48,
-    DoubleFoot2 = 49,
-    WallTopLeft = 124,
-    WallTopRight = 125,
-    WallBottomLeft = 151,
-    WallBottomRight = 152,
-    WallLeft = 175,
-    WallRight = 176,
-    WallTop = 202,
-    WallBottom = 203,
-    Stove = 323,
-    Sink = 322,
-    BigTv1 = 531,
-    BigTv2 = 532,
-    SmallTv = 536,
-    Couch1 = 500,
-    Couch2 = 501,
-    Couch3 = 502,
-    Recliner = 504,
-    BedsideTable = 478,
-    CoffeeTable = 505,
-    Chair = 477,
-    ShatteredGlass1 = 263,
-    ShatteredGlass2 = 290,
-    WoodPlank = 264,
-    Debris1 = 291,
-    Debris2 = 292,
-    OilSpill = 319,
-    KitchenTop = 320,
-    TreeTopLeft = 180,
-    TreeTopRight = 181,
-    TreeBottomLeft = 207,
-    TreeBottomRight = 208,
-    Bush1 = 182,
-    Bush2 = 209,
-    FallenLeaves = 212,
-    Box1 = 128,
-    Box2 = 129,
-    Box3 = 155,
-    Box4 = 156,
-    UWall = 123,
-    Marble = 11,
-    CornerTopLeft = 199,
-    CornerTopRight = 198,
-    CornerBottomRight = 171,
-    CornerBottomLeft = 172,
-    Nothing = usize::MAX,
-    Player = usize::MAX - 1,
-}
-
 #[derive(Component)]
 pub struct Tile;
 
 pub fn spawn_tile(
     cmds: &mut Commands,
     tile_pos: Vec2,
+    tile_z: f32,
     tile_tex_atlas: &Res<TextureAtlasOwner<Tile>>,
     tile_tex_idx: usize,
-    tile_z: f32,
-    tile_idx: TileIndex,
+    tile_lvl_obj: LevelObject,
 ) {
     cmds.spawn((
         Tile,
@@ -111,11 +34,11 @@ pub fn spawn_tile(
     ))
     .with_children(|parent| {
         if matches!(
-            tile_idx,
-            TileIndex::WallLeft
-                | TileIndex::WallBottomLeft
-                | TileIndex::WallTopLeft
-                | TileIndex::UWall
+            tile_lvl_obj,
+            LevelObject::WallLeft
+                | LevelObject::WallBottomLeft
+                | LevelObject::WallTopLeft
+                | LevelObject::UWall
         ) {
             parent.spawn((
                 TransformBundle::from_transform(Transform::from_xyz(
@@ -127,11 +50,11 @@ pub fn spawn_tile(
             ));
         }
         if matches!(
-            tile_idx,
-            TileIndex::WallRight
-                | TileIndex::WallBottomRight
-                | TileIndex::WallTopRight
-                | TileIndex::UWall
+            tile_lvl_obj,
+            LevelObject::WallRight
+                | LevelObject::WallBottomRight
+                | LevelObject::WallTopRight
+                | LevelObject::UWall
         ) {
             parent.spawn((
                 TransformBundle::from_transform(Transform::from_xyz(
@@ -143,8 +66,8 @@ pub fn spawn_tile(
             ));
         }
         if matches!(
-            tile_idx,
-            TileIndex::WallTop | TileIndex::WallTopLeft | TileIndex::WallTopRight
+            tile_lvl_obj,
+            LevelObject::WallTop | LevelObject::WallTopLeft | LevelObject::WallTopRight
         ) {
             parent.spawn((
                 TransformBundle::from_transform(Transform::from_xyz(
@@ -156,11 +79,11 @@ pub fn spawn_tile(
             ));
         }
         if matches!(
-            tile_idx,
-            TileIndex::WallBottom
-                | TileIndex::WallBottomLeft
-                | TileIndex::WallBottomRight
-                | TileIndex::UWall
+            tile_lvl_obj,
+            LevelObject::WallBottom
+                | LevelObject::WallBottomLeft
+                | LevelObject::WallBottomRight
+                | LevelObject::UWall
         ) {
             parent.spawn((
                 TransformBundle::from_transform(Transform::from_xyz(
@@ -171,7 +94,7 @@ pub fn spawn_tile(
                 Collider::cuboid(HALF_TILE_SIZE, HALF_WALL_THICKNESS),
             ));
         }
-        if matches!(tile_idx, TileIndex::CornerTopLeft) {
+        if matches!(tile_lvl_obj, LevelObject::CornerTopLeft) {
             parent.spawn((
                 TransformBundle::from_transform(Transform::from_xyz(
                     -HALF_TILE_SIZE - HALF_WALL_THICKNESS + WALL_THICKNESS,
@@ -181,7 +104,7 @@ pub fn spawn_tile(
                 Collider::cuboid(HALF_WALL_THICKNESS, HALF_WALL_THICKNESS),
             ));
         }
-        if matches!(tile_idx, TileIndex::CornerTopRight) {
+        if matches!(tile_lvl_obj, LevelObject::CornerTopRight) {
             parent.spawn((
                 TransformBundle::from_transform(Transform::from_xyz(
                     HALF_TILE_SIZE + HALF_WALL_THICKNESS - WALL_THICKNESS,
@@ -191,7 +114,7 @@ pub fn spawn_tile(
                 Collider::cuboid(HALF_WALL_THICKNESS, HALF_WALL_THICKNESS),
             ));
         }
-        if matches!(tile_idx, TileIndex::CornerBottomLeft) {
+        if matches!(tile_lvl_obj, LevelObject::CornerBottomLeft) {
             parent.spawn((
                 TransformBundle::from_transform(Transform::from_xyz(
                     -HALF_TILE_SIZE - HALF_WALL_THICKNESS + WALL_THICKNESS,
@@ -201,7 +124,7 @@ pub fn spawn_tile(
                 Collider::cuboid(HALF_WALL_THICKNESS, HALF_WALL_THICKNESS),
             ));
         }
-        if matches!(tile_idx, TileIndex::CornerBottomRight) {
+        if matches!(tile_lvl_obj, LevelObject::CornerBottomRight) {
             parent.spawn((
                 TransformBundle::from_transform(Transform::from_xyz(
                     HALF_TILE_SIZE + HALF_WALL_THICKNESS - WALL_THICKNESS,
